0d3220bf76c0f95b1527904ec43e3e90f2c6a939
subinDeveloper <subisubin322@gmail.com>
2024-10-17 15:27:26 +0530

    modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
+
 const Home = () => {
   return (
    <div>
-   
-  <div className="h-screen  ">
-    <img src="https://images.pexels.com/photos/27389956/pexels-photo-27389956/free-photo-of-a-long-table-with-plates-and-silverware-on-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='relative w-screen brightness-50' />
-    <div className=" flex justify-center items-center  flex-col lg:pt-20 lg:gap-6 absolute md:top-56 lg:top-96 top-40 text-white px-20">
-        <h1 className="lg:text-6xl text-2xl font-semibold text-center underline">Right Peoples Come To The Right Place</h1>
-       
-        <p className="lg:text-xl text-md font-bold text-center">Exceptional Service: Friendly, attentive, and personalized service from staff.Cleanliness and Hygiene: Impeccable cleanliness, sanitation, and hygiene standards.
-        Safety and Security: Robust security measures, secure parking, and in-room safes.
-       </p>
-       
-        <div className="flex justify-center py-4">
-      <Link to="/branchers" className="rounded-lg px-8 py-2  text-black">Find your plan</Link>
-      </div>
-        
-      </div>
+ <div className="bg-stone-800 h-screen w-full relative">
+  <img src="https://images.pexels.com/photos/27389956/pexels-photo-27389956/free-photo-of-a-long-table-with-plates-and-silverware-on-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" 
+  className="w-full h-full object-cover absolute mix-blend-overlay "/>
+
+  <div className="flex items-center justify-center flex-col pt-28 text-white gap-3">
+    <h1 className="font-bold text-3xl">Right Peoples Come To The Right Place</h1> 
+    <p className="text-xl" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, consequatur qui asperiores animi.</p>
+    <p className="text-xl" >Lorem ipsum dolor Officia accusamus aliquid tempora odio totam magnam voluptatem necessitatibus labore sequi illo.</p>
+    <p className="text-xl" >Lorem ipsum dolor eius voluptatum consequuntur, porro cupiditate amet dolorem impedit accusamus nesciunt praesentium cumque, distinctio, hic ratione mollitia?</p>
   </div>
+ </div>
+  {/* <div className='w-screen'>
+    <img src={img} alt=""  className='w-screen h-screen'/>
+  </div> */}
    </div>
   )
 }
